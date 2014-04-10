a08bc6fd7a9dd47d75be777cea23e61467f91016
Michael Forrest <michael.forrest@gmail.com>
2014-04-10 10:22:01 +0100

    Initial Commit

---
+++ UIColor+TomorrowThemeTests/UIColor_TomorrowThemeTests.m
@@ -0,0 +1,34 @@
+//
+//  UIColor_TomorrowThemeTests.m
+//  UIColor+TomorrowThemeTests
+//
+//  Created by Michael Forrest on 10/04/2014.
+//  Copyright (c) 2014 Good To Hear. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIColor_TomorrowThemeTests : XCTestCase
+
+@end
+
+@implementation UIColor_TomorrowThemeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
